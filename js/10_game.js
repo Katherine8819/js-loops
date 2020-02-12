cff9dd73ff76f371afa0b060b8f821016866d2dc
Katherine Zhdanova <katherine.zhdanova8@gmail.com>
2020-02-12 10:36:45 +0200

    guess game

--- js/10_game.js
+++ js/10_game.js
@@ -3,25 +3,21 @@
 
     let min = 0;
     let max = 100;
-    let current = 50;
-    let number = 50;
+    let current = 0;
 
     if (confirm("Загадайте число от 0 до 100")) {
-        for (let i = 0; current >= min && current <= max; i++) {
+        while (current >= min && current <= max) {
+            current = Math.floor(max - ((max - min) / 2));
             if (confirm(`Вы загадали ${current}?`)) {
-
+                alert(`Ура! Я угадал число ${current}`)
+                break
             } else {
                 if (confirm(`Число больше ${current}?`)) {
                     min = current;
-                    current = Math.round(current + (current * 0.5));
-                    console.log(`min ${min}, current ${current}`)
                 } else {
-                    max = current;
-                    current = Math.round(current - (current * 0.5));
-                    console.log(`min ${min}, current ${current}`)
+                    max = current
                 }
             }
-
 
         }
     }
@@ -29,5 +25,12 @@
 })()
 
 
+// if (confirm(`Число больше ${current}?`)) {
+            //     min = current;
+            //     current = Math.floor(current + (current - 50 / 2));
+            // } else {
+            //     min = current;
+            //     current = Math.floor(current - (current - 50 / 2));
+            // }
 
 // Игра «Угадай число». Предложить пользователю загадать число от 0 до 100 и отгадать его следующим способом: каждую итерацию цикла делите диапазон чисел пополам, записываете результат в N и спрашиваете у пользователя «Ваше число > N, < N или == N?». В зависимости от того, что указал пользователь, уменьшаете диапазон. Начальный диапазон от 0 до 100, поделили пополам и получили 50. Если пользователь указал, что его число > 50, то изменили диапазон на от 51 до 100. И так до тех пор, пока пользователь не выберет == N.
